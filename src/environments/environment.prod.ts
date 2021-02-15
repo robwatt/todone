4fadf89e07b402a19c494c418db9f7c1ea5352ea
Rob Watt <r.watt@rogers.com>
2021-02-14 19:42:42 -0500

    enable firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA08UQgN1dWOsYjQCGLI8ZhVQXjoABoR6c',
+    authDomain: 'todone-3f74b.firebaseapp.com',
+    projectId: 'todone-3f74b',
+    storageBucket: 'todone-3f74b.appspot.com',
+    messagingSenderId: '255752351527',
+    appId: '1:255752351527:web:2feccae076235e382bc302',
+    measurementId: 'G-334D9ZEFX8',
+  },
 };
