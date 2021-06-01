b94db5d4516f899e7741d515b11efab34cc52969
Rob Watt <rob.watt@outlook.com>
2021-05-31 22:47:53 -0400

    Add note page

---
+++ src/app/notes/note-list/note-list.component.ts
@@ -0,0 +1,27 @@
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-note-list',
+  templateUrl: './note-list.component.html',
+  styleUrls: ['./note-list.component.scss']
+})
+export class NoteListComponent implements OnInit {
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {}
+
+  /**
+   * This triggers the process to create a new note
+   */
+  createNote(): void {}
+}
